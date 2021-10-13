2119231213422151459a6dd2002b49f03d522538
fujiyum <91727339+fujiyum@users.noreply.github.com>
2021-10-13 10:35:01 +0000

    [4-10]Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'abouts/top'
-  get 'users/show'
-  get 'users/update'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
+  resource :books, only: [:index, :create, :show, :edit, :destroy]
+  resource :users, only: [:index, :show, :edit, :update]
 end
