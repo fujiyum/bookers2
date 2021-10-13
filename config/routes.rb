f23d839c12cf82d73475a5c63036e8da4658a554
fujiyum <91727339+fujiyum@users.noreply.github.com>
2021-10-13 17:03:23 +0000

    [4-10]Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'abouts/top'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
+  get 'users/show'
+  get 'users/edit'
+  get 'abouts/top'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :books, only: [:index, :create, :show, :edit, :destroy]
   resource :users, only: [:index, :show, :edit, :update]
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,8 +6,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get update" do
-    get users_update_url
+  test "should get edit" do
+    get users_edit_url
     assert_response :success
   end
 
