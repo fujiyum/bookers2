4f1fe90171aaa27c3ac9855426745cf93b691109
fujiyum <91727339+fujiyum@users.noreply.github.com>
2021-10-15 15:58:18 +0000

    [4-10]Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
 
 def index
+ @book = Book.new
 end
 
 def create
+ @book = Book.new(book_params)
+ @book.user_id = current_user.id
+ @book.save
+ redirect_to book_path(@book.id)
 end
 
 def show
@@ -17,8 +22,8 @@ end
 
 private
 
-def user_params
-    params.require(:user).permit(:profile_image, :name, :introduction)
+def book_params
+    params.require(:book).permit(:title, :body)
 end
 
 end
