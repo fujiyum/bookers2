2c6868ab8aee2dccddc6a8a74b6cd12a1ae42f5a
fujiyum <91727339+fujiyum@users.noreply.github.com>
2021-10-21 11:14:19 +0000

    [4-10]Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,12 +29,13 @@ def edit
 end
 
 def update
-book = Book.find(params[:id])
-if book.update(book_params)
- redirect_to book_path(book), notice: "You have updated book successfully."
-else
- render book_path(book)
-end
+ @book = Book.find(params[:id])
+ @book.user_id = current_user.id
+ if @book.update(book_params)
+  redirect_to book_path(@book.id), notice: "You have updated book successfully."
+ else
+  render :edit
+ end
 end
 
 def destroy
